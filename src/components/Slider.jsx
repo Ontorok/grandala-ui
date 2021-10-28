ba8b7871b6ac81336bd43b7205f234237612ed60
Ontorok <nasir.amath07@gmail.com>
2021-10-28 22:55:32 +0600

    Arrow button added on slider

--- src/components/Slider.jsx
+++ src/components/Slider.jsx
@@ -1,5 +1,16 @@
+import { ArrowLeftOutlined, ArrowRightOutlined } from '@mui/icons-material';
 import React from 'react';
+import { Arrow, Container } from './styled/Slider.styled';
 
-const Slider = () => <div />;
+const Slider = () => (
+    <Container>
+        <Arrow direction="left">
+            <ArrowLeftOutlined />
+        </Arrow>
+        <Arrow direction="right">
+            <ArrowRightOutlined />
+        </Arrow>
+    </Container>
+);
 
 export default Slider;
--- src/components/styled/Slider.styled.js
+++ src/components/styled/Slider.styled.js
@@ -4,6 +4,25 @@ export const Container = styled.div`
     width: 100%;
     height: 100vh;
     display: flex;
+    background-color: coral;
+    position: relative;
 `;
 
-export const Arrow = styled.span``;
+export const Arrow = styled.div`
+    width: 50px;
+    height: 50px;
+    background-color: #f7f0f0;
+    color: #000;
+    border-radius: 50%;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    position: absolute;
+    top: 0;
+    bottom: 0;
+    left: ${(props) => props.direction === 'left' && '10px'};
+    right: ${(props) => props.direction === 'right' && '10px'};
+    margin: auto;
+    cursor: pointer;
+    opacity: 0.5;
+`;
