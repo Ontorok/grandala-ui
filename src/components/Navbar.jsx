c6d568590b74c3d5877df59de5257fe9228a7e70
Ontorok <nasir.amath07@gmail.com>
2021-10-28 14:02:06 +0600

    nav section designed

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,6 +1,43 @@
+import { Search, ShoppingCartOutlined } from '@mui/icons-material';
+import { Badge } from '@mui/material';
 import React from 'react';
-import { Container } from './styled/Container.styles';
+import {
+    Center,
+    Container,
+    Input,
+    Language,
+    Left,
+    Logo,
+    MenuItem,
+    Right,
+    SearchContainer,
+    Wrapper
+} from './styled/Navbar.styled';
 
-const Navbar = () => <Container>Navbar</Container>;
+const Navbar = () => (
+    <Container>
+        <Wrapper>
+            <Left>
+                <Language>EN</Language>
+                <SearchContainer>
+                    <Input />
+                    <Search style={{ color: 'gray', fontSize: 16 }} />
+                </SearchContainer>
+            </Left>
+            <Center>
+                <Logo>DIGI SHOP.</Logo>
+            </Center>
+            <Right>
+                <MenuItem>REGISTER</MenuItem>
+                <MenuItem>SIGN IN</MenuItem>
+                <MenuItem>
+                    <Badge badgeContent={4} color="primary">
+                        <ShoppingCartOutlined />
+                    </Badge>
+                </MenuItem>
+            </Right>
+        </Wrapper>
+    </Container>
+);
 
 export default Navbar;
