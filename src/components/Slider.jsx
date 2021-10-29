94966eae67eca701fd2abe2f166c5a04b2aac63c
Ontorok <nasir.amath07@gmail.com>
2021-10-29 23:33:40 +0600

    Transition added on slider

--- src/components/Slider.jsx
+++ src/components/Slider.jsx
@@ -1,57 +1,57 @@
-import { ArrowLeftOutlined, ArrowRightOutlined } from '@mui/icons-material';
-import React from 'react';
-import sliderImage from '../assets/images/1.jpg';
-import sliderImage2 from '../assets/images/2.jpg';
+import { ArrowLeftOutlined, ArrowRightOutlined } from "@mui/icons-material";
+import React, { useState } from "react";
+import { sliderItems } from "../fake-data/slider";
 import {
-    Arrow,
-    Button,
-    Container,
-    Description,
-    Image,
-    ImageContainer,
-    InfoContainer,
-    Slide,
-    Title,
-    Wrapper
-} from './styled/Slider.styled';
+  Arrow,
+  Button,
+  Container,
+  Description,
+  Image,
+  ImageContainer,
+  InfoContainer,
+  Slide,
+  Title,
+  Wrapper
+} from "./styled/Slider.styled";
 
-const Slider = () => (
+const Slider = () => {
+  const [slideIndex, setSlideIndex] = useState(0);
+
+  const onSlideChange = (direction) => {
+    if (direction === "left") {
+      setSlideIndex(slideIndex > 0 ? slideIndex - 1 : sliderItems.length - 1);
+    } else {
+      setSlideIndex(slideIndex < sliderItems.length - 1 ? slideIndex + 1 : 0);
+    }
+  };
+  return (
     <Container>
-        <Arrow direction="left">
-            <ArrowLeftOutlined />
-        </Arrow>
+      <Arrow direction="left" onClick={() => onSlideChange("left")}>
+        <ArrowLeftOutlined />
+      </Arrow>
 
-        <Wrapper>
-            <Slide bgColor='F9FCF2'>
-                <ImageContainer>
-                    <Image src={sliderImage} />
-                </ImageContainer>
-                <InfoContainer>
-                    <Title>SUMMER SALE</Title>
-                    <Description>
-                        DON&#39;T COMPROMISE ON STYLE!! GET FLAT 30% OFF FOR NEW ARRIVALES!!!
-                    </Description>
-                    <Button>SHOP NOW</Button>
-                </InfoContainer>
+      <Wrapper slideIndex={slideIndex}>
+        {sliderItems.map((item, index) => {
+          return (
+            <Slide key={item.id} bgColor={item.bgColor}>
+              <ImageContainer>
+                <Image src={item.img} />
+              </ImageContainer>
+              <InfoContainer>
+                <Title>{item.title}</Title>
+                <Description>{item.description}</Description>
+                <Button>SHOP NOW</Button>
+              </InfoContainer>
             </Slide>
-            <Slide bgColor='ECFDF6'>
-                <ImageContainer>
-                    <Image src={sliderImage2} />
-                </ImageContainer>
-                <InfoContainer>
-                    <Title>SUMMER SALE</Title>
-                    <Description>
-                        DON&#39;T COMPROMISE ON STYLE!! GET FLAT 30% OFF FOR NEW ARRIVALES!!!
-                    </Description>
-                    <Button>SHOP NOW</Button>
-                </InfoContainer>
-            </Slide>
-        </Wrapper>
+          );
+        })}
+      </Wrapper>
 
-        <Arrow direction="right">
-            <ArrowRightOutlined />
-        </Arrow>
+      <Arrow direction="right" onClick={() => onSlideChange("right")}>
+        <ArrowRightOutlined />
+      </Arrow>
     </Container>
-);
+  );
+};
 
 export default Slider;
---
+++ src/fake-data/slider.js
@@ -0,0 +1,26 @@
+export const sliderItems = [
+  {
+    id: 1,
+    img: "https://i.ibb.co/pvK0dZF/1.jpg",
+    title: "SUMMER SALE 1",
+    description:
+      "DON'T COMPROMISE ON STYLE!! GET FLAT 30% OFF FOR NEW ARRIVALES!!!",
+    bgColor: "F9FCF2"
+  },
+  {
+    id: 2,
+    img: "https://i.ibb.co/56j2BD1/Man-holding-shopping-bags-with-presents-on-the-street.jpg",
+    title: "SUMMER SALE 2",
+    description:
+      "DON'T COMPROMISE ON STYLE!! GET FLAT 30% OFF FOR NEW ARRIVALES!!!",
+    bgColor: "ECFDF6"
+  },
+  {
+    id: 3,
+    img: "https://i.ibb.co/Cnck1yW/3.jpg",
+    title: "SUMMER SALE 3",
+    description:
+      "DON'T COMPROMISE ON STYLE!! GET FLAT 30% OFF FOR NEW ARRIVALES!!!",
+    bgColor: "FFEED7"
+  }
+];
