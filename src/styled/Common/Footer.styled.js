0b5ab2aecb8a27c026d9de1d3777654547e2def7
Ontorok <nasir.amath07@gmail.com>
2021-11-09 23:57:52 +0600

    Footer responsive

--- src/styled/Common/Footer.styled.js
+++ src/styled/Common/Footer.styled.js
@@ -1,84 +1,95 @@
-import styled from 'styled-components';
+import styled from "styled-components";
+import { devices } from "../../mediaQuery/devices";
 
 export const Container = styled.div`
-    display: flex;
+  display: flex;
+
+  @media ${devices.mobileM} {
+    flex-direction: column;
+  }
 `;
 
 export const Left = styled.div`
-    flex: 1;
-    display: flex;
-    flex-direction: column;
-    padding: 20px;
+  flex: 1;
+  display: flex;
+  flex-direction: column;
+  padding: 20px;
 `;
 
 export const Logo = styled.h1``;
 
 export const Description = styled.p`
-    margin: 20px 0;
+  margin: 20px 0;
+  text-align: justify;
+  word-break: keep-all;
 `;
 
 export const SocialContainer = styled.div`
-    display:flex;
-  
+  display: flex;
 `;
 
 export const SocialIcon = styled.div`
-    width:40px;
-    height:40px;
-    border-radius: 50%;
-    margin-right:20px;
-    background-color: #${props => props.bgColor};
-    color:#fff;
-    display:flex;
-    align-items: center;
-    justify-content: center;
-    transition: all 1s ease;
-    &:hover{
-        transform: scale(1.1)
-    }
+  width: 40px;
+  height: 40px;
+  border-radius: 50%;
+  margin-right: 20px;
+  background-color: #${(props) => props.bgColor};
+  color: #fff;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  transition: all 1s ease;
+  &:hover {
+    transform: scale(1.1);
+  }
 `;
 
-
 export const Center = styled.div`
-    flex: 1;
-    padding: 20px;
+  flex: 1;
+  padding: 20px;
+
+  @media ${devices.mobileM} {
+    display: none;
+  }
 `;
 
 export const Title = styled.h3`
-    margin-bottom: 30px;
+  margin-bottom: 30px;
 `;
 
 export const List = styled.ul`
-    margin: 0;
-    padding: 0;
-    list-style: none;
-    display: flex;
-    flex-wrap:wrap;
+  margin: 0;
+  padding: 0;
+  list-style: none;
+  display: flex;
+  flex-wrap: wrap;
 `;
 
 export const ListItem = styled.li`
-    width: 50%;
-    margin-bottom: 10px;
-`
+  width: 50%;
+  margin-bottom: 10px;
+`;
 
 export const Right = styled.div`
-    flex: 1;
-    padding: 20px;
+  flex: 1;
+  padding: 20px;
+
+  @media ${devices.mobileM} {
+    background-color: rgba(228, 240, 192, 0.2);
+  }
 `;
 
 export const ContactItem = styled.div`
-    argin-bottom: 20px;
-    display: flex;
-    align-items: center;
-    margin-bottom: 10px;
+  argin-bottom: 20px;
+  display: flex;
+  align-items: center;
+  margin-bottom: 10px;
 `;
 
 export const ContactItemDesc = styled.span`
-    margin-left: 10px;
+  margin-left: 10px;
 `;
 
 export const Payment = styled.img`
-   margin-top: 10px;
+  margin-top: 10px;
 `;
-
-
