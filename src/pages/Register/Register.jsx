4d31a790080aaf140a3065540c1840bd18f8166c
Ontorok <nasir.amath07@gmail.com>
2021-11-14 01:04:06 +0600

    Register style separated

--- src/pages/Register/Register.jsx
+++ src/pages/Register/Register.jsx
@@ -1,67 +1,14 @@
-import { devices } from "mediaQuery/devices";
 import React, { Fragment } from "react";
 import { Outlet } from "react-router-dom";
-import styled from "styled-components";
-
-const Container = styled.div`
-  width: 100vw;
-  height: 100vh;
-  background: linear-gradient(
-      rgba(255, 255, 255, 0.5),
-      rgba(255, 255, 255, 0.5)
-    ),
-    url("https://i.ibb.co/F3vzpbt/landback.jpg") center;
-  background-size: cover;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-`;
-
-const Wrapper = styled.div`
-  width: 40%;
-  padding: 20px;
-  background-color: #fff;
-  border-radius: 10px;
-
-  @media ${devices.mobileM} {
-    width: 75%;
-  }
-  @media ${devices.tablet} {
-    width: 75%;
-  }
-`;
-
-const Title = styled.h1`
-  font-size: 24px;
-  font-weight: 300;
-`;
-
-const Form = styled.form`
-  display: flex;
-  flex-wrap: wrap;
-`;
-
-const Input = styled.input`
-  flex: 1;
-  min-width: 40%;
-  margin: 20px 10px 0 0;
-  padding: 10px;
-  border-radius: 5px;
-`;
-
-const Agreement = styled.span`
-  font-size: 12px;
-  margin: 20px 0;
-`;
-
-const Button = styled.button`
-  width: 40%;
-  border: none;
-  padding: 15px 20px;
-  background-color: teal;
-  color: white;
-  cursor: pointer;
-`;
+import {
+  Agreement,
+  Button,
+  Container,
+  Form,
+  Input,
+  Title,
+  Wrapper
+} from "./Register.styled";
 
 const Register = () => {
   return (
