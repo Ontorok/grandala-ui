919cea02ec5c189d08cb261f5a574b56345a2450
Ontorok <nasir.amath07@gmail.com>
2021-11-14 01:01:50 +0600

    Login style separated

--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -1,69 +1,13 @@
-import { devices } from "mediaQuery/devices";
 import React, { Fragment } from "react";
-import styled from "styled-components";
-
-const Container = styled.div`
-  width: 100vw;
-  height: 100vh;
-  background: linear-gradient(
-      rgba(255, 255, 255, 0.5),
-      rgba(255, 255, 255, 0.5)
-    ),
-    url("https://i.ibb.co/YyHCzLk/loginback.jpg") center;
-  background-size: cover;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-`;
-
-const Wrapper = styled.div`
-  width: 25%;
-  padding: 20px;
-  background-color: rgba(255, 255, 255, 0.9);
-  border-radius: 10px;
-
-  @media ${devices.mobileM} {
-    width: 75%;
-  }
-  @media ${devices.tablet} {
-    width: 75%;
-  }
-`;
-
-const Title = styled.h1`
-  font-size: 24px;
-  font-weight: 300;
-`;
-
-const Form = styled.form`
-  display: flex;
-  flex-direction: column;
-`;
-
-const Input = styled.input`
-  flex: 1;
-  min-width: 40%;
-  margin: 10px 0;
-  padding: 10px;
-  border-radius: 5px;
-`;
-
-const Button = styled.button`
-  width: 40%;
-  border: none;
-  padding: 15px 20px;
-  background-color: teal;
-  color: white;
-  cursor: pointer;
-  margin-bottom: 10px;
-`;
-
-const Link = styled.a`
-  margin: 5px 0;
-  font-size: 12px;
-  text-decoration: underline;
-  cursor: pointer;
-`;
+import {
+  Button,
+  Container,
+  Form,
+  Input,
+  Link,
+  Title,
+  Wrapper
+} from "./Login.Styled";
 
 const Login = () => {
   return (
