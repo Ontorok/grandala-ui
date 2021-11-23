559abb2e1b16cb5c1660088952db5fecdbeb055f
Ontorok <nasir.amath07@gmail.com>
2021-11-23 23:54:20 +0600

    add quantity change, size change, color change functionality in product details

--- src/pages/ProductDetail/ProductDetails.jsx
+++ src/pages/ProductDetail/ProductDetails.jsx
@@ -1,8 +1,10 @@
 import { Add, Remove } from "@mui/icons-material";
+import axios from "axios";
 import Footer from "parts/Footer/Footer";
 import Newsletter from "parts/Newsletter/Newsletter";
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
+import { http } from "services/config";
 import {
   AddContainer,
   Amount,
@@ -25,52 +27,104 @@ import {
 } from "./ProductDetails.styled";
 
 const ProductDetails = () => {
-  const param = useParams();
+  //#region HOOKS
+  const { id } = useParams();
+  //#endregion
+
+  //#region STATES
+  const [product, setProduct] = useState({});
+  const [isPageLoaded, setIsPageLoaded] = useState(false);
+  const [quantity, setQuantity] = useState(1);
+  const [size, setSize] = useState("");
+  const [color, setColor] = useState(" ");
+  //#endregion
+
+  //#region EFFECTS
+  /**
+   * fetch products from db by caterogy wise
+   **/
+  useEffect(() => {
+    const source = axios.CancelToken.source();
+    const token = source.token;
+
+    const fetchProducts = async () => {
+      try {
+        const product = await http.get(`/product/find/${id}`, {
+          cancelToken: token
+        });
+        setProduct(product.data.data);
+        setIsPageLoaded(true);
+      } catch (error) {
+        console.log(error.message);
+      }
+    };
+
+    fetchProducts();
+
+    return () => {
+      source.cancel();
+    };
+  }, [id]);
+  //#endregion
+
+  //#region EVENTS
+  const handleQuantityChange = (direction) => {
+    if (direction === "inc") {
+      setQuantity((prev) => prev + 1);
+    } else {
+      setQuantity((prev) => (prev === 1 ? 1 : prev - 1));
+    }
+  };
+
+  const handleAddToCart = () => {
+    console.log({ color, size, quantity });
+  };
+  //#endregion
   return (
     <Container>
-      <Wrapper>
-        <ImageContainer>
-          <Image src="https://i.ibb.co/HV8jrKV/denimjumpsuit.jpg" />
-        </ImageContainer>
-        <InfoCotainer>
-          <Title>{param.productName}</Title>
-          <Description>
-            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Minima
-            quam rem temporibus ea, laboriosam sit nobis quia illum quos
-            repudiandae quisquam, cum soluta pariatur asperiores impedit maiores
-            delectus, deserunt voluptates aliquam et in voluptatum veniam?
-            Reprehenderit ducimus similique iure facere. Repellat dolorum,
-            minima consequuntur officiis dolore vero sequi veritatis magnam!
-          </Description>
-          <Price>$ 20</Price>
-          <FilerContainer>
-            <Filter>
-              <FilterTitle>Color</FilterTitle>
-              <FilterColor color="black" />
-              <FilterColor color="darkblue" />
-              <FilterColor color="grey" />
-            </Filter>
-            <Filter>
-              <FilterTitle>Size</FilterTitle>
-              <FilterSize>
-                <FilterSizeOption>XS</FilterSizeOption>
-                <FilterSizeOption>S</FilterSizeOption>
-                <FilterSizeOption>M</FilterSizeOption>
-                <FilterSizeOption>L</FilterSizeOption>
-                <FilterSizeOption>XL</FilterSizeOption>
-              </FilterSize>
-            </Filter>
-          </FilerContainer>
-          <AddContainer>
-            <AmountContainer>
-              <Remove />
-              <Amount>1</Amount>
-              <Add />
-            </AmountContainer>
-            <Button>ADD TO CART</Button>
-          </AddContainer>
-        </InfoCotainer>
-      </Wrapper>
+      {isPageLoaded ? (
+        <Wrapper>
+          <ImageContainer>
+            <Image src={product.img} />
+          </ImageContainer>
+          <InfoCotainer>
+            <Title>{product.title}</Title>
+            <Description>{product.desc}</Description>
+            <Price>$ {product.price}</Price>
+            <FilerContainer>
+              <Filter>
+                <FilterTitle>Color</FilterTitle>
+                {product.color.map((item) => (
+                  <FilterColor
+                    key={item}
+                    color={item}
+                    onClick={() => setColor(item)}
+                  />
+                ))}
+              </Filter>
+              <Filter>
+                <FilterTitle>Size</FilterTitle>
+                <FilterSize onChange={(e) => setSize(e.target.value)}>
+                  {product.size.map((item) => (
+                    <FilterSizeOption key={item}>{item}</FilterSizeOption>
+                  ))}
+                </FilterSize>
+              </Filter>
+            </FilerContainer>
+            <AddContainer>
+              <AmountContainer>
+                <Remove onClick={() => handleQuantityChange("dec")} />
+                <Amount>{quantity}</Amount>
+                <Add onClick={() => handleQuantityChange("inc")} />
+              </AmountContainer>
+              <Button onClick={handleAddToCart}>ADD TO CART</Button>
+            </AddContainer>
+          </InfoCotainer>
+        </Wrapper>
+      ) : (
+        <p> Item Loading.... </p>
+      )}
+
       <Newsletter />
       <Footer />
     </Container>
