5250bdf5456d893897837f0d7002e5fd91a47125
Ontorok <nasir.amath07@gmail.com>
2021-11-27 22:09:17 +0600

    persist user and cart

--- src/index.js
+++ src/index.js
@@ -2,14 +2,17 @@ import React from "react";
 import reactDom from "react-dom";
 import { Provider } from "react-redux";
 import { BrowserRouter } from "react-router-dom";
-import store from "redux/store/store";
+import { PersistGate } from "redux-persist/integration/react";
+import { persistor, store } from "redux/store/store";
 import App from "./App";
 
 reactDom.render(
-  <BrowserRouter>
-    <Provider store={store}>
-      <App />
-    </Provider>
-  </BrowserRouter>,
+  <Provider store={store}>
+    <PersistGate loading={null} persistor={persistor}>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </PersistGate>
+  </Provider>,
   document.getElementById("root")
 );
--- src/redux/store/store.js
+++ src/redux/store/store.js
@@ -3,20 +3,46 @@
  * Description: Global Application State
  * Author: Nasir Ahmed
  * Date: 24-November-2021
- * Modified: 26-November-2021
+ * Modified: 27-November-2021
  **/
 
-import { configureStore } from "@reduxjs/toolkit";
+import { combineReducers, configureStore } from "@reduxjs/toolkit";
+import {
+  FLUSH,
+  PAUSE,
+  PERSIST,
+  persistReducer,
+  persistStore,
+  PURGE,
+  REGISTER,
+  REHYDRATE
+} from "redux-persist";
+import storage from "redux-persist/lib/storage";
+import cartSlice from "redux/slices/cartSlice";
 import userSlice from "redux/slices/userSlice";
-import cartSlice from "../slices/cartSlice";
 
-export default configureStore({
-  reducer: {
-    cart: cartSlice,
-    user: userSlice
-  }
+const persistConfig = {
+  key: "root",
+  version: 1,
+  storage
+};
+
+const rootReducer = combineReducers({ user: userSlice, cart: cartSlice });
+const persistedReducer = persistReducer(persistConfig, rootReducer);
+
+export const store = configureStore({
+  reducer: persistedReducer,
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({
+      serializableCheck: {
+        ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER]
+      }
+    })
 });
+
+export const persistor = persistStore(store);
 
 /** Change Log
  * 26-Nov-2021 : add userSlice
+ * 27-Nov-2021 : persist current info and cart
  **/
