147157a715ad8a10affbe1fdc84f7a2c294ad032
Ontorok <nasir.amath07@gmail.com>
2022-04-06 00:22:39 +0600

    update grandala ui

--- src/index.js
+++ src/index.js
@@ -1,17 +1,14 @@
 import React from "react";
-import reactDom from "react-dom";
+import ReactDOM from "react-dom";
 import { Provider } from "react-redux";
-import { BrowserRouter } from "react-router-dom";
-import { PersistGate } from "redux-persist/integration/react";
-import { persistor, store } from "redux/store/store";
+import { PersistGate } from 'redux-persist/integration/react';
 import App from "./App";
+import { persistor, store } from "./redux/store";
 
-reactDom.render(
+ReactDOM.render(
   <Provider store={store}>
     <PersistGate loading={null} persistor={persistor}>
-      <BrowserRouter>
-        <App />
-      </BrowserRouter>
+      <App />
     </PersistGate>
   </Provider>,
   document.getElementById("root")
